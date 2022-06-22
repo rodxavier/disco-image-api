ec1e6fd3dcfd38395f64bf551e40ed4080a81d35
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2022-06-22 11:58:45 +0800

    Add can_generate_expiring_links field to Plan model

--- image_api/accounts/models.py
+++ image_api/accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Plan(models.Model):
     name = models.CharField(max_length=40)
     presets = models.ManyToManyField("images.ImagePreset", related_name="plans")
+    can_generate_expiring_links = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
