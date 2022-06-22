d0edd14acb7e4d524dd2e53cb2d376400de9709e
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2022-06-22 11:35:17 +0800

    Add accounts app

---
+++ image_api/accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    plan = models.ForeignKey("Plan", on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
