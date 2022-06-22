6238ae01fd94a8324f4a724226f4b27dbca62401
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2022-06-22 12:21:38 +0800

    Add API endpoints

--- image_api/image_api/urls.py
+++ image_api/image_api/urls.py
@@ -2,9 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
 ]
 
