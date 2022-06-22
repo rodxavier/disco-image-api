7f3472502bb73f40ed3f03387287cb4f43ae3ee6
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2022-06-22 11:28:30 +0800

    Update database settings

--- image_api/image_api/settings.py
+++ image_api/image_api/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,9 +79,13 @@ WSGI_APPLICATION = 'image_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB", default="image_api_db"),
+        "USER": env("POSTGRES_USER", default="image_api_user"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="image_api_password"),
+        "HOST": env("POSTGRES_HOST", default="localhost"),
+        "PORT": env("POSTGRES_PORT", default=5432),
     }
 }
 
