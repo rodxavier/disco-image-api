bea8714e792946ab5e3e621a0af26f1e35520069
Rod Xavier Bondoc <rod.xavier.bondoc@gmail.com>
2022-06-22 11:37:51 +0800

    Add Plan and User model to django admin

--- image_api/accounts/admin.py
+++ image_api/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Plan, User
+
+UserAdmin.list_display += ("plan",)
+UserAdmin.fieldsets += (("Plan", {"fields": ("plan",)}),)
+
+
+admin.site.register(Plan)
+admin.site.register(User, UserAdmin)
